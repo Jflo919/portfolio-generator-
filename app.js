c5b921d3cfb31e48e0ae2db63ecad129d9820577
JulianF <floydjulian1991@gmail.com>
2022-03-22 18:00:17 -0700

    adding file system and html output

--- app.js
+++ app.js
@@ -1,26 +1,32 @@
-const animalArray = ['dog', 'cat', 'pig'];
-
-animalArray.push('cow');
-
-const profileDataArr = [
-     'Floyd' , 'Web Developer'
-    
-];
+const profileDataArgs = process.argv.slice(2, process.argv.length);
+const [names, github] = profileDataArgs;
 
 
 
-// Notice the lack of parentheses around the 'profileDataArr' parameter?
-
-const printProfileData = (profileDataArr) => {
-  for (let i = 0; i < profileDataArr.length; i+= 1) { 
-    console.log(profileDataArr[i]);
-  }
-  console.log('===========');
-  // is the same as this...
-  profileDataArr.forEach((profileItem) => {
-    console.log(profileItem)
-  });
-
-
+const generatePage = (names, github) => {
+  return `
+  <!DOCTYPE html>
+  <html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title> Portfolio Demo </title>
+  </head>
+  
+  <body>
+    <h1>${names}</h1>
+    <h2><a href="https://github.com/${github}">Github</a></h2>
+  </body>
+  </html>  
+  `;  
 };
-    printProfileData(profileDataArr);
+
+fs.writeFile('index.html', generatePage(names,github), err => {
+  if (err) throw err;
+
+  console.log("Portfolio Complete! Check out index.html to see the output!");
+});
+
+
+
